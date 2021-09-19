94b8abc0398bb8719ff308170765d29e888fffce
Myoui Mina <ramandadeny7@gmail.com>
2021-09-19 15:30:34 +0700

    done filters

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,52 @@
 /*global $,Swiper, navigator*/
 (function () {
 $(document).ready(function() {
-    function initSwiperProducts(){
+    let swiperFilters;
+    let swiperTabs;
+    let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    function initSwiperJumbotron(){
         let swiper = new Swiper('.swiper--jumbotron', {
             effect: 'slide',
-            loop: true,
             centeredSlides: true,
+            loop: true,
+            watchOverflow: true,
             slidesPerView: 1.2,
             spaceBetween: 0,
             navigation: {
                 nextEl: '.jumbotron--next',
                 prevEl: '.jumbotron--prev',
+            },
+            breakpoints: {
+                640: {
+                    slidesPerView: 1.1,
+                    centeredSlides: false,
+                }
+            }
+        });
+    }
+    function initSwiperFilters(){
+        swiperFilters = new Swiper('.swiper--filters', {
+            effect: 'slide',
+            watchOverflow: true,
+            slidesPerView: 1,
+            spaceBetween: 0,
+            allowTouchMove: false,
+            noSwiping: true,
+            speed: 600
+        });
+    }
+    function initSwiperTabFilters(){
+        swiperTabs = new Swiper('.swiper--tabFilters', {
+            effect: 'slide',
+            watchOverflow: true,
+            slidesPerView: 1,
+            spaceBetween: 0,
+            allowTouchMove: false,
+            noSwiping: true,
+            speed: 600,
+            navigation: {
+                nextEl: '.tab-filters--next',
+                prevEl: '.tab-filters--prev',
             },
         });
     }
@@ -22,6 +58,33 @@ $(document).ready(function() {
 
         sidebarStatus=!sidebarStatus
     })
-    initSwiperProducts();
+    $('#dateFFO').datepicker({
+        uiLibrary: 'bootstrap4'
+    })
+    $('#dateMRE').datepicker({
+        uiLibrary: 'bootstrap4'
+    })
+    $('#dateStayHD').datepicker({
+        uiLibrary: 'bootstrap4'
+    })
+    $('#dateStayVO').datepicker({
+        uiLibrary: 'bootstrap4'
+    })
+    $('.filter-menu').each(function(idx) {
+        $(this).click(() => {
+            $('.filter-menu').removeClass('active')
+            $(this).addClass('active')
+            swiperFilters.slideTo(idx)
+        })
+    })
+    initSwiperJumbotron();
+    initSwiperFilters();
+    if(isMobile) {
+        initSwiperTabFilters()
+        swiperTabs.on('slideChange', function () {
+            swiperFilters.slideTo(swiperTabs.activeIndex)
+            $('.filter-menu').removeClass('active')
+        });
+    }
 });
 }(jQuery));
